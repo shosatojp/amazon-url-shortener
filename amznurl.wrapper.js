5425d03758eb7f02b50a231394fe06473b616f38
shosatojp <me@shosato.jp>
2020-04-16 01:31:32 +0900

    changename `shorter -> shortenizer`

--- amznurl.wrapper.js
+++ amznurl.wrapper.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Amazon URL Shorter (Debug)
+// @name         Amazon URL Shortenizer (Debug)
 // @match        *://www.amazon.co.jp/*
 // @match        *://www.amazon.com/*
-// @require      file:///home/sho/repos/amazon-url-shorter/amznurl.user.js
+// @require      file:///home/sho/repos/amazon-url-shortenizer/amznurl.user.js
 // @run-at       document-start
 // @noframes
 // ==/UserScript==
